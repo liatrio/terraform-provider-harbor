4ce2c287771c0a05150d76a154b9bb7b67b4734d
Alice Jones <21381891+Pactionly@users.noreply.github.com>
2020-03-26 21:23:47 -0700

    Add support for robot accounts

--- go.mod
+++ go.mod
@@ -4,5 +4,6 @@ go 1.12
 
 require (
 	github.com/hashicorp/errwrap v1.0.0
+	github.com/hashicorp/terraform v0.12.24
 	github.com/hashicorp/terraform-plugin-sdk v1.4.1
 )
---
+++ harbor/robot_account.go
@@ -0,0 +1,72 @@
+package harbor
+
+import (
+	"encoding/json"
+	"fmt"
+)
+
+type RobotAccountCreate struct {
+	Access      []RobotAccountAccess `json:"access,omitempty"`
+	Name        string               `json:"name,omitempty"`
+	Description string               `json:"description,omitempty"`
+}
+
+type RobotAccountPostRep struct {
+	Token string `json:"token"`
+	Name  string `json:"name"`
+}
+
+type RobotAccountUpdate struct {
+	Disabled bool `json:"disabled,omitempty"`
+}
+
+type RobotAccount struct {
+	Description  string `json:"description"`
+	UpdateTime   string `json:"update_time"`
+	CreationTime string `json:"creation_time"`
+	ExpiresAt    int    `json:"expires_at"`
+	Disabled     bool   `json:"disabled"`
+	ProjectID    int    `json:"project_id"`
+	ID           int    `json:"id"`
+	Name         string `json:"name"`
+}
+
+type RobotAccountAccess struct {
+	Action   string `json:"action",omitempty`
+	Resource string `json:"resource",omitempty`
+}
+
+func (client *Client) GetRobotAccount(id string) (*RobotAccount, error) {
+	var robot *RobotAccount
+
+	err := client.get(id, &robot, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	return robot, nil
+}
+
+func (client *Client) NewRobotAccount(projectID string, robot *RobotAccountCreate) (*RobotAccountPostRep, string, error) {
+	body, location, err := client.post(fmt.Sprintf("%s/robots", projectID), robot)
+	if err != nil {
+		return nil, "", err
+	}
+
+	var response *RobotAccountPostRep
+
+	err = json.Unmarshal(body, &response)
+	if err != nil {
+		return nil, "", err
+	}
+
+	return response, location, nil
+}
+
+func (client *Client) UpdateRobotAccount(id string, robot *RobotAccountUpdate) error {
+	return client.put(id, robot)
+}
+
+func (client *Client) DeleteRobotAccount(id string) error {
+	return client.delete(id, nil)
+}
