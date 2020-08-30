5de443ca9fe2967cac3c10deee5f2830a2be44cf
Alice Jones <21381891+Pactionly@users.noreply.github.com>
2020-08-30 10:51:18 -0700

    Lock testing Helm release to Harbor 1.x version

--- scripts/helmRelease.sh
+++ scripts/helmRelease.sh
@@ -1,6 +1,6 @@
 helm status terraform-provider-harbor-acctest 2>/dev/null
 if [ $? == 1 ]; then
-  helm install terraform-provider-harbor-acctest harbor/harbor --set expose.type=nodePort,expose.tls.enabled=false
+  helm install terraform-provider-harbor-acctest harbor/harbor --set expose.type=nodePort,expose.tls.enabled=false --version "1.3.4"
 fi
 
 echo
