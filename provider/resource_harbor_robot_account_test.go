c294c708e385555b4e643aa337fcbeab3be7a0a6
Alice Jones <21381891+Pactionly@users.noreply.github.com>
2020-08-30 10:48:38 -0700

    Add/Update acceptance testing for Harbor robot account

--- provider/resource_harbor_robot_account_test.go
+++ provider/resource_harbor_robot_account_test.go
@@ -6,11 +6,13 @@ import (
 
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/acctest"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+	"github.com/liatrio/terraform-provider-harbor/harbor"
 )
 
-func TestAccHarborRobotAccountBasic(t *testing.T) {
+func TestAccHarborRobotAccount_Basic(t *testing.T) {
 	projectName := "terraform-" + acctest.RandString(10)
 	robotName := "robot$terraform-" + acctest.RandString(10)
+	resourceName := "harbor_robot_account.robot"
 
 	resource.Test(t, resource.TestCase{
 		Providers:    testAccProviders,
@@ -18,38 +20,106 @@ func TestAccHarborRobotAccountBasic(t *testing.T) {
 		CheckDestroy: testCheckResourceDestroy("harbor_robot_account"),
 		Steps: []resource.TestStep{
 			{
-				Config: testHarborRobotAccountBasic(projectName, robotName, "false"),
-				Check:  testCheckResourceExists("harbor_robot_account.robot"),
-			},
-		},
-	})
-}
-
-func TestAccHarborRobotAccountUpdate(t *testing.T) {
-	projectName := "terraform-" + acctest.RandString(10)
-	robotName := "robot$terraform-" + acctest.RandString(10)
-
-	resource.Test(t, resource.TestCase{
-		Providers:    testAccProviders,
-		PreCheck:     func() { testAccPreCheck(t) },
-		CheckDestroy: testCheckResourceDestroy("harbor_robot_account"),
-		Steps: []resource.TestStep{
-			{
-				Config: testHarborRobotAccountBasic(projectName, robotName, "false"),
-				Check:  testCheckResourceExists("harbor_robot_account.robot"),
-			},
-			{
-				Config: testHarborRobotAccountBasic(projectName, robotName, "true"),
-				Check: resource.ComposeTestCheckFunc(
-					testCheckResourceExists("harbor_robot_account.robot"),
-					resource.TestCheckResourceAttr("harbor_robot_account.robot", "disabled", "true"),
+				Config: testCreateHarborRobotAccount_Basic(projectName, robotName, "false"),
+				Check: resource.ComposeAggregateTestCheckFunc(
+					testCheckResourceExists(resourceName),
+					resource.TestCheckResourceAttrSet(resourceName, "token"),
 				),
 			},
 		},
 	})
 }
 
-func testHarborRobotAccountBasic(projectName string, robotName string, disabled string) string {
+func TestAccHarborRobotAccount_Full(t *testing.T) {
+	projectName := "terraform-" + acctest.RandString(10)
+	robotName := "robot$terraform-" + acctest.RandString(10)
+	resourceName := "harbor_robot_account.robot"
+
+	resource.Test(t, resource.TestCase{
+		Providers:    testAccProviders,
+		PreCheck:     func() { testAccPreCheck(t) },
+		CheckDestroy: testCheckResourceDestroy("harbor_robot_account"),
+		Steps: []resource.TestStep{
+			{
+				Config: testCreateHarborRobotAccount_Full(
+					projectName,
+					robotName,
+					"A Test Robot Account",
+					"true",
+				),
+				Check: resource.ComposeAggregateTestCheckFunc(
+					testCheckResourceExists(resourceName),
+					resource.TestCheckResourceAttr(resourceName, "name", robotName),
+					resource.TestCheckResourceAttr(resourceName, "description", "A Test Robot Account"),
+					resource.TestCheckResourceAttr(resourceName, "disabled", "true"),
+					resource.TestCheckResourceAttrSet(resourceName, "token"),
+				),
+			},
+		},
+	})
+}
+
+func TestAccHarborRobotAccount_Update(t *testing.T) {
+	projectName := "terraform-" + acctest.RandString(10)
+	robotName := "robot$terraform-" + acctest.RandString(10)
+	resourceName := "harbor_robot_account.robot"
+
+	resource.Test(t, resource.TestCase{
+		Providers:    testAccProviders,
+		PreCheck:     func() { testAccPreCheck(t) },
+		CheckDestroy: testCheckResourceDestroy("harbor_robot_account"),
+		Steps: []resource.TestStep{
+			{
+				Config: testCreateHarborRobotAccount_Basic(projectName, robotName, "false"),
+				Check:  testCheckResourceExists(resourceName),
+			},
+			{
+				Config: testCreateHarborRobotAccount_Basic(projectName, robotName, "true"),
+				Check: resource.ComposeTestCheckFunc(
+					testCheckResourceExists(resourceName),
+					resource.TestCheckResourceAttr(resourceName, "disabled", "true"),
+				),
+			},
+		},
+	})
+}
+
+func TestAccHarborRobotAccount_CreateAfterManualDestroy(t *testing.T) {
+	var robotId string
+
+	projectName := "terraform-" + acctest.RandString(10)
+	robotName := "robot$terraform-" + acctest.RandString(10)
+	resourceName := "harbor_robot_account.robot"
+
+	resource.Test(t, resource.TestCase{
+		Providers:    testAccProviders,
+		PreCheck:     func() { testAccPreCheck(t) },
+		CheckDestroy: testCheckResourceDestroy("harbor_robot_account"),
+		Steps: []resource.TestStep{
+			{
+				Config: testCreateHarborRobotAccount_Basic(projectName, robotName, "false"),
+				Check: resource.ComposeAggregateTestCheckFunc(
+					testCheckResourceExists(resourceName),
+					testCheckGetResourceId(resourceName, &robotId),
+				),
+			},
+			{
+				PreConfig: func() {
+					client := testAccProvider.Meta().(*harbor.Client)
+
+					err := client.DeleteRobotAccount(robotId)
+					if err != nil {
+						t.Fatal(err)
+					}
+				},
+				Config: testCreateHarborRobotAccount_Basic(projectName, robotName, "true"),
+				Check:  testCheckResourceExists(resourceName),
+			},
+		},
+	})
+}
+
+func testCreateHarborRobotAccount_Basic(projectName string, robotName string, disabled string) string {
 	return fmt.Sprintf(`
 resource "harbor_project" "project" {
 	name     = "%s"
@@ -65,4 +135,36 @@ resource "harbor_robot_account" "robot" {
 	}
 }
 	`, projectName, robotName, disabled)
+}
+
+func testCreateHarborRobotAccount_Full(projectName string, robotName string, description string, disabled string) string {
+	return fmt.Sprintf(`
+resource "harbor_project" "project" {
+	name     = "%s"
+}
+
+resource "harbor_robot_account" "robot" {
+	project_id = harbor_project.project.id
+
+	name = "%s"
+	description = "%s"
+	disabled = %s
+	access {
+		resource = "image"
+		action = "pull"
+	}
+	access {
+		resource = "image"
+		action = "push"
+	}
+	access {
+		resource = "helm-chart"
+		action = "pull"
+	}
+	access {
+		resource = "helm-chart"
+		action = "push"
+	}
+}
+	`, projectName, robotName, description, disabled)
 }
