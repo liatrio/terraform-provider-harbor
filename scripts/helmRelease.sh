c733dd284df9fa9a170645bdadfe2369f315911d
Alice Jones <21381891+Pactionly@users.noreply.github.com>
2021-02-16 10:19:26 -0800

    Update local development instructions

--- scripts/helmRelease.sh
+++ scripts/helmRelease.sh
@@ -1,3 +1,4 @@
+helm repo add harbor https://helm.goharbor.io
 helm status terraform-provider-harbor-acctest 2>/dev/null
 if [ $? == 1 ]; then
   helm install terraform-provider-harbor-acctest harbor/harbor --set expose.type=loadBalancer,notary.enabled=false,expose.tls.enabled=false,externalURL=http://localhost:80 --version "1.5.3"
